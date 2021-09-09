0bb09e83dec59f3a71e6689025ed1213947ceb42
mileny <mileny.marinho@estudante.ifms.edu.br>
2021-09-09 01:15:37 -0400

    Finalização do HEADER

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
@@ -8,8 +8,14 @@ export default function App() {
       <View style={styles.header}>
           <Image source={require("./src/assets/arrow-left.png")}>
           </Image>
+      <View style={styles.inputTextView}>
           <Image source={require("./src/assets/ei_search.png")}>
           </Image>
+          <TextInput placeholder="Search"
+          placeholderTextColor="#F5FFFF"
+          style={styles.textInput}>
+          </TextInput>
+      </View>
           <Image source={require("./src/assets/share.png")}>
           </Image>
       </View>
@@ -25,10 +31,25 @@ const styles = StyleSheet.create({
   },
   header: {
     minHeight:93,
-    backgroundColor: "#2C4877",
+    backgroundColor: "#4369B0",
     paddingTop:40,
     flexDirection:"row",
     justifyContent:"space-between",
     paddingHorizontal:30,
-  }
+  },
+  inputTextView:{
+    flex:1,
+    backgroundColor: "#2C4877",
+    height:40,
+    marginHorizontal:10,
+    alignItems:"center",
+    borderRadius:28,
+    flexDirection:"row",
+  },
+  textInput:{
+    color:"#F5FFFF",
+    //maxWidth:200,
+    flex:1,
+    paddingRight:10,
+  },
 });
