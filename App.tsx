301d62b5679b98f0124bd339859e8d81d49d5962
mileny <mileny.marinho@estudante.ifms.edu.br>
2021-09-09 00:10:27 -0400

    incluindo imagens do projeto

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+          <Image source={require("./src/assets/arrow-left.png")}>
+          </Image>
+          <Image source={require("./src/assets/ei_search.png")}>
+          </Image>
+          <Image source={require("./src/assets/share.png")}>
+          </Image>
+      </View>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,9 +21,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    height:93,
+    backgroundColor: "#2C4877",
+    paddingTop:40,
+  }
 });
